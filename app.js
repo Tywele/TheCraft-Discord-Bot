ed3d46cfc1c6af42307dd72ca0f7953026489ec2
Tywele <bingel.simon@gmail.com>
2018-05-30 23:47:01 +0200

    Added command to setgame and send messages to other channels

--- app.js
+++ app.js
@@ -2,17 +2,38 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
 
+// react to starting the bot
 client.on('ready', () => {
     console.log('I\'m online\nI\'m online');
 });
 
+// react to messages
 var prefix = '+';
-
 client.on('message', message => {
-    if (message.author === client.user) return;
-    if (message.content.startsWith('ping')) {
-        message.channel.send('ping');
+    // stop when message has no prefix
+    if (!message.content.startsWith(prefix)) return;
+
+    // get cmd args
+    let args = message.content.split(' ').slice(1);
+    let argresult = args.join(' '); 
+
+    // stop bot from responding to itself
+    if (message.author.bot) return;
+
+    if (message.content.startsWith(prefix + 'ping')) {
+        message.channel.send(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
+    } else 
+
+    // set game of bot
+    if (message.content.startsWith(prefix + 'setgame')) {
+        client.user.setActivity(argresult);
+    } else
+
+    // send message to different channel
+    if (message.content.startsWith(prefix + 'send')) {
+        client.channels.get('140508702284185600').send('Hello from bot-sandbox');
     }
 });
 
+// login the bot
 client.login(settings.token);
